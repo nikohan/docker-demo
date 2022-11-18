019ede3450f6251106d970d19287b01ba5bbea9d
zhaohan <zhaohan@jd.com>
2022-11-18 17:29:58 +0800

    add go

---
+++ go/main.go
@@ -0,0 +1,23 @@
+package main
+ 
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "OK")
+}
+
+func main() {
+    http.HandleFunc("/", index)
+	
+	port := 80
+ 
+    err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+	log.Printf("start http health on port: %d", port)
+}
