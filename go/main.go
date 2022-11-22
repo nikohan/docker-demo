6f9c3aa8f0624500521a1bf66c1b383db8ac4072
zhaohan <zhaohan@jd.com>
2022-11-22 16:02:54 +0800

    增加http请求

--- go/main.go
+++ go/main.go
@@ -1,23 +1,33 @@
 package main
- 
+
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "OK")
+	fmt.Fprintf(w, "OK")
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	res, _ := http.Get("https://www.google.com/")
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Fprintf(w, string(body))
 }
 
 func main() {
-    http.HandleFunc("/health", health)
-	
+	http.HandleFunc("/health", health)
+	http.HandleFunc("/get", get)
+
 	port := 80
- 
-    err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 	log.Printf("start http health on port: %d", port)
 }
